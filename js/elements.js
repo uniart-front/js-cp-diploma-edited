9edefd7097a67c332a8ad4e749e0f92fbc3557c9
uniart-front <109544062+uniart-front@users.noreply.github.com>
2023-07-02 20:26:09 +0500

    Update elements.js

--- js/elements.js
+++ js/elements.js
@@ -1,58 +1,38 @@
-let days = Array.from(document.getElementsByClassName("page-nav__day")); // устанавливаем дату в список выше
-let date = new Date();
-date.setDate(date.getDate() - 1);
-
-days.forEach(element => { // дата на следующие дни
-  date.setDate(date.getDate() +1);
-  element.children[1].textContent = date.getDate();
-  switch(Number(date.getDay())){
-    case 1:{
-      element.children[0].textContent = "Пн";
-      break;
-    }
-    case 2:{
-      element.children[0].textContent = "Вт";
-      break;
-    }
-    case 3:{
-      element.children[0].textContent = "Ср";
-      break;
-    }
-    case 4:{
-      element.children[0].textContent = "Чт";
-      break;
-    }
-    case 5:{
-      element.children[0].textContent = "Пт";
-      break;
-    }
-    case 6:{
-      element.children[0].textContent = "Сб";
-      element.classList.add("page-nav__day_weekend");
-      break;
-    }
-    case 0:{
-      element.children[0].textContent = "Вс";
-      element.classList.add("page-nav__day_weekend");
-      break;
-    }
-    default:
-      break;
+export function createElement(tagName, className, parent) {
+    const element = document.createElement(tagName);
+    element.className = className;
+    parent.append(element);
+    return element;
   }
-})
-
-let currentDate = new Date();// текущая дата
-let selectDay = new Date();// день который выбрали 
-let navDay = document.getElementsByClassName("page-nav")[0];
-navDay.getElementsByClassName("page-nav__day_chosen")[0].classList.toggle("page-nav__day_chosen");// выключаем выбранный день по умолчанию 
-navDay.children[0].classList.toggle("page-nav__day_chosen")// сегодняшний день делаем активным
-
-let pageNavDays = Array.from(document.getElementsByClassName("page-nav__day"));// смена текущей даты
-pageNavDays.forEach((e,index) => {
-  e.onclick = function () {
-    document.getElementsByClassName("page-nav__day_chosen")[0].classList.toggle("page-nav__day_chosen");// выключаем текущий
-    e.classList.toggle("page-nav__day_chosen");// ставим выбранный 
-    selectDay.setDate(currentDate.getDate()+index);
-    setFilms(JSON.parse(localStorage.getItem("info")),selectDay);// функция из udateFilm.js обновляем всю информацию под выбранную дату
+  
+  /**
+   * Создает разметку для навигационных элементов с датами.
+   *
+   * @param {object} date экземпляр объекта Date.
+   * @return {string} сформированную строку разметки.
+   */
+  export function createDateElementHTML(date) {
+    const day = date.getDate();
+    const weekday = date.toLocaleString('ru-Ru', {
+      weekday: 'short',
+    });
+  
+    return `
+      <span class="page-nav__day-week">${weekday}</span>
+      <span class="page-nav__day-number">${day}</span>
+    `;
   }
-})
+  
+  /**
+   * Добавляем дата-атрибуты для навигационных элементов с датами.
+   *
+   * @param {object} elemnt элемент разметки, которому необходимо добавить атрибуты
+   * @param {number} year год в числовом формате
+   * @param {number} month месяц в числовом формате
+   * @param {number} day месяц в числовом формате
+   */
+  export function createDateElementDataAttributes(elemnt, year, month, day) {
+    elemnt.dataset.year = year;
+    elemnt.dataset.month = month;
+    elemnt.dataset.day = day;
+  }
