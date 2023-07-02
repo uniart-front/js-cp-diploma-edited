8d5b73d30a2bb11fdcd801c9a86c61997c2450c6
uniart-front <109544062+uniart-front@users.noreply.github.com>
2023-07-02 18:47:56 +0500

    Update ticket.js

--- js/ticket.js
+++ js/ticket.js
@@ -1,35 +1,51 @@
-let selectedMovie = JSON.parse(localStorage.getItem('selectedMovie'));
-let selectChairs = JSON.parse(localStorage.getItem('selectChairs'));
+let wrap = document.getElementsByClassName("ticket__info-wrapper")[0];// контейнер с информацией
 
-const ticketTitle = document.querySelector('.ticket__title');
-ticketTitle.textContent = selectedMovie.movieName;
+let tickets = JSON.parse(sessionStorage.getItem(sessionStorage.getItem("data-seance-id")));
+wrap.getElementsByClassName("ticket__details ticket__title")[0].textContent = tickets.currentBuy["data-film-name"];//имя фильма
 
-const ticketChairs = document.querySelector('.ticket__chairs');
-ticketChairs.textContent = selectChairs.chairs;
+let ticketChairs = wrap.getElementsByClassName("ticket__details ticket__chairs")[0]; // ряд место
+ticketChairs.textContent = "";
+Object.keys(tickets.currentBuy["chair"]).forEach((row,index) => {
+    tickets.currentBuy["chair"][row].forEach((place,index) => {
+        ticketChairs.textContent += `${row}/${place}`;
+        if(tickets.currentBuy["chair"][row].length - 1 > index){
+            ticketChairs.textContent += ", ";
+        }  
+    })
+    if(Object.keys(tickets.currentBuy["chair"]).length - 1 > index){
+        ticketChairs.textContent += ", ";
+    }
+})
+wrap.getElementsByClassName("ticket__details ticket__hall")[0].textContent = tickets.currentBuy["data-hall-name"].substr(tickets.currentBuy["data-hall-name"].length - 1);// номер зала
+wrap.getElementsByClassName("ticket__details ticket__start")[0].textContent = tickets.currentBuy["data-seance-time"]; //начало сеанса
 
-const ticketHall = document.querySelector('.ticket__hall');
-ticketHall.textContent = selectedMovie.hallName.slice(4);
+// Получаем элемент по индексу
+let childElement = wrap.children[3]; // после 3 элемента вставляем qr(0,1,2,3,qr...)
 
-const ticketStart = document.querySelector('.ticket__start');
-ticketStart.textContent = selectedMovie.seanceTime;
+let div = document.createElement("div"); // блок для qr кода
 
-const ticketInfoQr = document.querySelector('.ticket__info-qr');
-const stringQR = `Фильм: ${selectedMovie.movieName} Зал: ${selectedMovie.hallName} Места: ${selectChairs.chairs} Сеанс: ${selectedMovie.seanceId} Дата: ${selectedMovie.seanceDay}`;
+document.getElementsByClassName("ticket__info-qr")[0].remove(); // удаляем стандартный qr;
 
-const qrcode = QRCreator(stringQR, {
-  mode: 4,
+let textQr = `Фильм: ${tickets.currentBuy["data-film-name"]}\nРяд/Место: ${ticketChairs.textContent}\nЗал: ${tickets.currentBuy["data-hall-name"].substr(tickets.currentBuy["data-hall-name"].length - 1)}\nНачало: ${tickets.currentBuy["data-seance-time"]}`;
+JSON.stringify(textQr);
+// добавляем свой
+let qr = QRCreator(textQr,
+{ mode: 4,
   eccl: 0,
   version: 8,
-  mask: 2,
+  mask: 3,
   image: 'png',
   modsize: 4,
-  margin: 0,
+  margin: 0
 });
-
-const content = (qrcode) => {
-  return qrcode.error
-    ? `недопустимые исходные данные ${qrcode.error}`
-    : qrcode.result;
+const content = (qrcode) =>{
+  return qrcode.error ?
+    `недопустимые исходные данные ${qrcode.error}`:
+    qrcode.result;
 };
-
-ticketInfoQr.append(content(qrcode));
+div.classList.add("ticket__info-qr");
+let qrCanvas = content(qr);
+qrCanvas.style.padding = "5px";
+qrCanvas.style.background = "white";
+div.appendChild(qrCanvas);// заносим qr в div
+wrap.insertBefore(div, childElement.nextSibling);// после 3 элемента вставляем блок div с qr
