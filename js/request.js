4b33598cbb6597a03e11083c198a60bca3734427
uniart-front <109544062+uniart-front@users.noreply.github.com>
2023-07-02 18:54:24 +0500

    Update request.js

--- js/request.js
+++ js/request.js
@@ -1,31 +1,22 @@
-/**
- * Делает HTTP-запрос к серверу для получения данных.
- *
- * @param {string} metod метод запроса
- * @param {string} url (URL)
- * @param {string} body  запрос
- * @return {object} Promise, содержащий полученные данные
- */
- export function sendRequest(metod, url, body = null) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-  
-      xhr.open(metod, url);
-      xhr.responseType = 'json';
-      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  
-      xhr.onload = () => {
-        if (xhr.status >= 400) {
-          reject(xhr.response);
-        } else {
-          resolve(xhr.response);
+function request(callback,data) {
+    let url = "https://jscp-diplom.netoserver.ru/"; // URL для обращения
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); 
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          callback(xhr.responseText);
+        } 
+        else {
+          alert("Ошибка: " + xhr.status);
         }
-      };
-  
-      xhr.onerror = () => {
-        reject(xhr.response);
-      };
-  
-      xhr.send(body);
-    });
+      }
+    };
+
+    xhr.onerror = function() {
+      alert("Произошла ошибка сети или неправильный URL");
+    };
+
+    xhr.send(data);
   }
