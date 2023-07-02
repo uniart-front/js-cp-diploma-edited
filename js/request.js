9c5c528ae2d9f8597b8af8f145bde74a64b3fdd1
uniart-front <109544062+uniart-front@users.noreply.github.com>
2023-07-02 22:09:49 +0500

    Update request.js

--- js/request.js
+++ js/request.js
@@ -1,31 +1,14 @@
-/**
- * Делает HTTP-запрос к серверу для получения данных.
- *
- * @param {string} metod Метод запроса.
- * @param {string} url Адрес (URL)
- * @param {string} body Тело запроса.
- * @return {object} Promise, содержащий полученные данные.
- */
-export function sendRequest(metod, url, body = null) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-
-    xhr.open(metod, url);
-    xhr.responseType = 'json';
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    xhr.onload = () => {
-      if (xhr.status >= 400) {
-        reject(xhr.response);
-      } else {
-        resolve(xhr.response);
-      }
-    };
-
-    xhr.onerror = () => {
-      reject(xhr.response);
-    };
-
-    xhr.send(body);
+async function postResponse(url = '', data = {}) {
+  const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: data
   });
+  if (!response.ok) {
+    throw Error;
+  } else {
+    return await response.json();
+  }
 }
